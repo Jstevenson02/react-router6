bc10740c0cf75f1e2f244d80940069e034417190
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-24 12:00:16 -0500

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
