6ea3ca639db743e0710002490da4d69acb9df2bb
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-24 12:17:10 -0500

    Project setup, easy routes and links setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,36 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+function Home() {
+  return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Hello Home! 😃</h1>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <h1>About Jake,</h1>
+      <ul>
+        <li>Likes to code</li>
+        <li>Has 2 pet dogs</li>
+        <li>Has a lovely girlfriend</li>
+      </ul>
     </>
   );
 }
